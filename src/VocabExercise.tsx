a5d14760e50f23017bbe56511bc294a4bb13d3f6
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-11-26 20:53:48 +0100

    Add question for vocab exercises

--- src/VocabExercise.tsx
+++ src/VocabExercise.tsx
@@ -6,9 +6,12 @@ class VocabExercise extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
 
+        const shuffledVocab = getTriplyShuffledArray(vocab.words);
+
         this.state = {
             inputText: '',
-            shuffledVocab: getTriplyShuffledArray(vocab.words),
+            shuffledVocab,
+            question: shuffledVocab[0].fr,
             correctCount: 0,
             wrongCount: 0,
             currentIndex: 0
@@ -21,12 +24,23 @@ class VocabExercise extends React.Component<Props, State> {
 
     handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (this.state.currentIndex < this.state.shuffledVocab.length - 1) {
+            this.setState(state => ({
+                currentIndex: state.currentIndex + 1,
+                question: this.state.shuffledVocab[this.state.currentIndex + 1].fr,
+            }))
+        } else {
+            this.setState(() => ({
+                question: "Terminé !"
+            }));
+        }
         console.log(this.state.inputText);
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
+                <h2>{this.state.question}</h2>
                 <input type="text" value={this.state.inputText} onChange={this.handleChange} />
                 <input type="submit" value="Valider" />
             </form>
@@ -41,6 +55,7 @@ type Props = {};
 type State = {
     inputText: string,
     shuffledVocab: VocabLine[],
+    question: string,
     correctCount: number,
     wrongCount: number,
     currentIndex: number,
