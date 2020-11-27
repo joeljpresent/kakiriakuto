cb529787468ce112031f5190d05dfb79c8dafb70
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-11-27 13:13:59 +0100

    Add fields "title" and "video_id" to Vojaq files

--- src/utils.tsx
+++ src/utils.tsx
@@ -15,12 +15,22 @@ export function getTriplyShuffledArray<T>(array: T[]) {
     ];
 }
 
+/** Data about a given vocabulary course */
 export type VocabFile = {
+    /** Title of the vocabulary course */
+    title: string,
+    /** The YouTube ID of the video for the vocabulary course (e.g., `dQw4w9WgXcQ`) */
+    video_id: string,
+    /** The words taught in the vocabulary course */
     words: VocabLine[];
 };
 
+/** A vocabulary word with its translation and other information */
 export type VocabLine = {
+    /** The Japanese word */
     jap: string;
+    /** The romanized transcription of the Japanese word */
     romaji: string;
+    /** The French translation of the word */
     fr: string;
 };
--- src/vocabs.tsx
+++ src/vocabs.tsx
@@ -1,8 +1,9 @@
 import { VocabFile } from "./utils";
 import conjonctions from './vocab/conjonctions.json';
 
+/** Array of all vocabulary files */
 const vocabs: VocabFile[] = [
-    conjonctions
+    conjonctions,
 ];
 
 export default vocabs;
