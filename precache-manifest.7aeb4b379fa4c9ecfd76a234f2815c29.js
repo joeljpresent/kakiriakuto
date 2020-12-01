2471f4ff75ae9147278a546e24184c790cd4e8af
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-12-01 18:49:46 +0100

    Updates

---
+++ precache-manifest.7aeb4b379fa4c9ecfd76a234f2815c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5cbe7bf45d1fa8eefceac0ba562e80",
+    "url": "/kakiriakuto/index.html"
+  },
+  {
+    "revision": "a242e2696dd124ac3641",
+    "url": "/kakiriakuto/static/css/main.865cad58.chunk.css"
+  },
+  {
+    "revision": "8f53cd0805ced3c29ee2",
+    "url": "/kakiriakuto/static/js/2.0cf6d125.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/kakiriakuto/static/js/2.0cf6d125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8495fceba4c9f04928ee",
+    "url": "/kakiriakuto/static/js/3.d9eec844.chunk.js"
+  },
+  {
+    "revision": "a242e2696dd124ac3641",
+    "url": "/kakiriakuto/static/js/main.f135f3ac.chunk.js"
+  },
+  {
+    "revision": "c84f6f4de9bd6b3e628e",
+    "url": "/kakiriakuto/static/js/runtime-main.3ff6677a.js"
+  }
+]);
