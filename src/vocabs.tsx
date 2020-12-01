048c8b0b1c689d3900709c35f6c538b93d99daf6
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-12-01 18:20:17 +0100

    Remove e_supesharu from the list of vocab lists

--- src/vocabs.tsx
+++ src/vocabs.tsx
@@ -2,7 +2,6 @@ import { VocabFile } from "./utils";
 import annexe from './vocab/annexe.json';
 import conjonctions from './vocab/conjonctions.json';
 import premiers_kanji from './vocab/premiers_kanji.json';
-import e_supesharu from './vocab/e_supesharu.json';
 import yasashii from "./vocab/yasashii.json"
 
 /** Array of all vocabulary files */
@@ -10,7 +9,6 @@ const vocabs: VocabFile[] = [
     premiers_kanji,
     conjonctions,
     annexe,
-    e_supesharu,
     yasashii,
 ];
 
