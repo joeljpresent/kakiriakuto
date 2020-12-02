247f942ad9b14a3518148d99571c7e44a3dd3cd4
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-12-02 14:26:16 +0100

    Add vocab "suffixes honorifiques"

--- src/vocabs.tsx
+++ src/vocabs.tsx
@@ -2,12 +2,14 @@ import { VocabFile } from "./utils";
 import annexe from './vocab/annexe.json';
 import conjonctions from './vocab/conjonctions.json';
 import premiers_kanji from './vocab/premiers_kanji.json';
+import suffixes_honorifiques from './vocab/suffixes_honorifiques.json';
 import yasashii from "./vocab/yasashii.json"
 
 /** Array of all vocabulary files */
 const vocabs: VocabFile[] = [
     premiers_kanji,
     conjonctions,
+    suffixes_honorifiques,
     annexe,
     yasashii,
 ];
