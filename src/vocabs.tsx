ecd250f03030b55ca3d809d65e799c11128e87e1
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2020-12-03 12:36:16 +0100

    Add vocab "Unités de mesure"

--- src/vocabs.tsx
+++ src/vocabs.tsx
@@ -4,12 +4,14 @@ import conjonctions from './vocab/conjonctions.json';
 import premiers_kanji from './vocab/premiers_kanji.json';
 import pronoms_personnels from './vocab/pronoms_personnels.json';
 import suffixes_honorifiques from './vocab/suffixes_honorifiques.json';
+import unites_mesure from './vocab/unites_mesure.json';
 import yasashii from "./vocab/yasashii.json"
 
 /** Array of all vocabulary files */
 const VOCABS: VocabFile[] = [
     premiers_kanji,
     conjonctions,
+    unites_mesure,
     pronoms_personnels,
     suffixes_honorifiques,
     annexe,
